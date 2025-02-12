f1807ad0d442173784d9a41aba2fb3d000d745cf
Zenith John <zenith-john@foxmail.com>
2025-02-12 14:03:23 +0800

    Add function from characters to unicode.

--- src/components/UnicodeComp.vue
+++ src/components/UnicodeComp.vue
@@ -1,8 +1,10 @@
 <script lang="ts">
+
 export default {
   data() {
     return {
-      code: '',
+      unicode: '',
+      chars: '',
     }
   },
   mounted() { },
@@ -24,20 +26,47 @@ export default {
     hexToChar(code: string) {
       let num = parseInt(code, 16);
       return String.fromCharCode(num);
+    },
+    codify(chars: string) {
+      let ret = "";
+      for (var i = 0; i < chars.length; i++) {
+        if (i != 0) {
+          ret = ret + " ";
+        }
+        ret += chars.charCodeAt(i).toString(16);
+      }
+      return ret;
+    },
+    update_unicode(event: Event) {
+      if (event.target !== null) {
+        this.unicode = (event.target as HTMLInputElement).value;
+      }
+      this.chars = this.decode(this.unicode);
+    },
+    update_chars(event: Event) {
+      if (event.target !== null) {
+        this.chars = (event.target as HTMLInputElement).value;
+      }
+      this.unicode = this.codify(this.chars);
     }
   }
 }
 </script>
 
 <template>
-  <label for="code">
-    Enter Unicode separated by space:
-    <input v-model="code" placeholder="Enter the code." />
-  </label>
   <div>
-    <pre>{{ decode(code) }}</pre>
+    <label for="unicode">
+      Unicode separated by space:
+      <input v-bind:value="unicode" @input="event => update_unicode(event)" />
+    </label>
   </div>
-  <div> For example: 6807 51C6 => 标准</div>
+  <div>
+    <label for="chars">
+      Characters:
+      <input v-bind:value="chars" @input="event => update_chars(event)" />
+    </label>
+  </div>
+  <div> For example: 6807 51C6 <=> 标准</div>
 </template>
 
 <style></style>
