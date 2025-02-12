40a6deb9f0085dda118ad82a0d6b2ff77e730722
Zenith John <zenith-john@foxmail.com>
2025-02-12 10:45:14 +0800

    Add a binary tool.

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ import { RouterLink, RouterView } from 'vue-router'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/sudoku">Sudoku</RouterLink>
+        <RouterLink to="/binary">Binary</RouterLink>
       </nav>
     </div>
   </header>
